59330c12b0565f28a569495a57db150ec87deff5
Ubuntu <ubuntu@ip-172-31-28-168.us-east-2.compute.internal>
2021-08-24 13:51:53 +0000

    change proper column names

--- customers.sql
+++ customers.sql
@@ -8,10 +8,10 @@ final AS
 ( 
     SELECT 
         customers.customer_id AS customer_id
-        , customers.name AS name
-        , customers.email AS email
+        , customers.name AS customer_name
+        , customers.email AS customer_email
         , MIN(orders.created_at) AS first_order_at
-        , COUNT(*) AS number_of_orders 
+        , COUNT(*) AS number_of_orders
     FROM 
         customers 
     JOIN 
