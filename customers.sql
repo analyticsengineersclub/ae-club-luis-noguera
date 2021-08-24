523bf23041c99be0bb316208ce4b7574da1be23e
Ubuntu <ubuntu@ip-172-31-28-168.us-east-2.compute.internal>
2021-08-24 14:02:57 +0000

    update aliases

--- customers.sql
+++ customers.sql
@@ -7,17 +7,17 @@ orders AS (SELECT * FROM `analytics-engineers-club.coffee_shop.orders`),
 final AS 
 ( 
     SELECT 
-        customers.customer_id AS customer_id
-        , customers.name AS name
-        , customers.email AS email
-        , MIN(orders.created_at) AS first_order_at
+        c.customer_id AS customer_id
+        , c.name AS name
+        , c.email AS email
+        , MIN(co.created_at) AS first_order_at
         , COUNT(*) AS number_of_orders 
     FROM 
-        customers 
+        customers AS c
     JOIN 
-        orders 
+        orders AS co
     ON 
-        customers.id = orders.customer_id
+        c.id = co.customer_id
     GROUP BY 1, 2, 3
     ORDER BY first_order_at
     LIMIT 5
